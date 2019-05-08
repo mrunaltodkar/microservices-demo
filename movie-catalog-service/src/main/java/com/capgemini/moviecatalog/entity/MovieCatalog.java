421d570713afe93f7a960c821b708bab400f3214
Todkar <mrunal-sunilkumar.todkar@capgemini.com>
2019-05-08 17:49:20 +0530

    Added microservices-demo

---
+++ movie-catalog-service/src/main/java/com/capgemini/moviecatalog/entity/MovieCatalog.java
@@ -0,0 +1,45 @@
+package com.capgemini.moviecatalog.entity;
+
+public class MovieCatalog {
+
+	private String movieName;
+	private String movieDescription;
+	private double movieRating;
+
+	public MovieCatalog() {
+		super();
+		
+	}
+
+	public MovieCatalog(String movieName, String movieDescription, double movieRating) {
+		super();
+		this.movieName = movieName;
+		this.movieDescription = movieDescription;
+		this.movieRating = movieRating;
+	}
+
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+
+	public String getMovieDescription() {
+		return movieDescription;
+	}
+
+	public void setMovieDescription(String movieDescription) {
+		this.movieDescription = movieDescription;
+	}
+
+	public double getMovieRating() {
+		return movieRating;
+	}
+
+	public void setMovieRating(double movieRating) {
+		this.movieRating = movieRating;
+	}
+
+}
